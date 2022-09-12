c173ab9401da559e8b963870ba681b4818fe838c
kcons <193791789@qq.com>
2022-09-12 19:03:05 +0800

    ✨ feat: 全局配置

--- src/app/index.js
+++ src/app/index.js
@@ -3,8 +3,21 @@
  * @Date: 2022-09-09 07:35:40
  */
 
-const Koa = require('koa2')
+const Koa = require('koa')
+
+const Router = require('koa-router')
 
 const app = new Koa()
+
+const userRouter = new Router({ prefix: '/users' })
+
+userRouter.post('/', (ctx, next) => {
+  console.log('12345');
+  ctx.body = '创建用户成功~'
+})
+
+app.use(userRouter.routes())
+
+app.use(userRouter.allowedMethods())
 
 module.exports = app
